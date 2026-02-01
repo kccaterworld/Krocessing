18d5ec3a87e7e7d88e48e015ec25e99ee9efafe0
Kemal Cater <kc@caterworld.xyz>
2026-02-01 11:39:35 -0500

    Added cleanup of class files

--- build.sh
+++ build.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-
-# Clean old class files
 find . -name "*.class" -delete 2>/dev/null
-
-# Find all Java files and compile them
 SOURCES=$(find . -name "*.java")
-
 javac $SOURCES
-
-# Check if compilation succeeded
 if [ $? -ne 0 ]; then
     echo "Compilation failed!"
     exit 1
 fi
-
-# Create JAR with all class files
 jar cfe krocessing.jar krocessing.Main -C . krocessing
-
 echo "Build successful! Run with: java -jar krocessing.jar"
+find . -name "*.class" -delete 2>/dev/null
