3f58dd03f1945c832ac8281ebb11b0b7d7b57cee
Kemal <kc@caterworld.xyz>
2026-02-01 19:18:44 -0500

    Fixed cross platform version issues

--- build.bat
+++ build.bat
@@ -8,6 +8,6 @@ if %errorlevel% neq 0 (
     echo Compilation failed!
     exit /b 1
 )
-jar cfe krocessing.jar krocessing.Main -C . krocessing
-echo Build successful! Run with: java -jar krocessing.jar
+jar cfe krocessingWindows.jar krocessing.Main -C . krocessing
+echo Build successful! Run with: java -jar krocessingWindows.jar
 for /r %%f in (*.class) do del "%%f" 2>nul
