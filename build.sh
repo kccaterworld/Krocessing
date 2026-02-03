23a8d065d2aeca68e93f0c46d3c6c0d31be52036
Kemal Cater <kc@caterworld.xyz>
2026-02-02 23:33:42 -0500

    Fixed classname to find Main

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ if [ $? -ne 0 ]; then
     echo "Compilation failed!"
     exit 1
 fi
-jar cfe krocessingArch.jar krocessingArch.Main -C . krocessing
+jar cfe krocessingArch.jar krocessing.Main -C . krocessing
 echo "Build successful! Run with: java -jar krocessingArch.jar"
 find . -name "*.class" -delete 2>/dev/null
